3b872b22da741f22850e8488347da99fb6ffe28a
joseph-serrao <josephserrrao42@gmail.com>
2022-07-14 17:44:11 +0530

    rewrote code
    A portion of the code has been re-written to a different solution. The code works successfully unlike before.

--- visual_memory.py
+++ visual_memory.py
@@ -23,22 +23,68 @@ try:
 finally:
     start_button.click()
 
-squares = {}
+squares = []
 squares_class = []
 a = 'square active'
-sequence = []
+grid_length = 3
 
-for i in range(1, 4):
-    for j in range(1, 4):
-        squares.append(driver.find_element(
-            By.XPATH, f"//div[@class='css-hvbk5q eut2yre0']/div[{i}]/div[{j}]"))
 
-while True:
-    squares_class = [elem.get_attribute("class")[0] for elem in squares]
-    print(squares_class)
+def getSquares(n):
+    squares.clear()
+    for i in range(1, n):
+        for j in range(1, n):
+            squares.append(driver.find_element(
+                By.XPATH, f"//div[@class='css-hvbk5q eut2yre0']/div[{i}]/div[{j}]"))
+
+
+getSquares(grid_length + 1)
+
+
+sequence = {}
+
+level = driver.find_element(By.XPATH, "//span[@class='css-dd6wi1']/span[2]")
+level_no = 0
+count = 0
+
+start_time = float('inf')
+end_time = 2.5
+wait_complete = False
+
+grid_increased_count = 1
+level_to_increase = 3
+
+while level_no <= 40:
+    level_no = int(level.text)
+
+    if level_no > count:
+        if not wait_complete:
+            start_time = time.time()
+            wait_complete = True
+
+        if level_no == level_to_increase:
+            grid_length += 1
+            getSquares(grid_length + 1)
+            grid_increased_count += 1
+            level_to_increase = (
+                ((grid_increased_count ** 2) + (6 * grid_increased_count) + 2) // 3)
+
+        count += 1
+
+    if wait_complete:
+        squares_class = [elem.get_attribute("class")[0] for elem in squares]
 
     for i in range(len(squares_class)):
         if squares_class[i] == 'a':
-            sequence.append(i)
+            2
+            sequence[str(i)] = i
 
-    print(sequence)
+    if (time.time() - start_time) > end_time:
+
+        for i in sequence.values():
+            squares[i].click()
+
+        wait_complete = False
+
+        start_time = float('inf')
+
+        sequence.clear()
