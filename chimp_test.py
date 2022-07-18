87384c8bc08021ed7d980264f7c9815edc84fd8a
joseph-serrao <josephserrrao42@gmail.com>
2022-07-18 21:58:00 +0530

    added chimp test

---
+++ chimp_test.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+import chromedriver_autoinstaller
+
+chromedriver_autoinstaller.install()
+
+link = "https://humanbenchmark.com/tests/chimp"
+
+driver = webdriver.Chrome()
+driver.get(link)
+
+start_button = None
+
+try:
+    start_button = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located(
+            (By.XPATH, "//button[@class='css-de05nr e19owgy710' and contains(text(), 'Start')]"))
+    )
+finally:
+    start_button.click()
+
+for i in range(4, 42):
+    for j in range(1, i+1):
+        driver.find_element(
+            By.CSS_SELECTOR, f"div[data-cellnumber='{j}']").click()
+
+    driver.find_element(By.CSS_SELECTOR, ".css-de05nr.e19owgy710").click()
