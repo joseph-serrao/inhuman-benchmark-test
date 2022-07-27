c6d2159ef811d09a9e3c3c4c1c539bc29f9aa901
joseph-serrao <josephserrrao42@gmail.com>
2022-07-27 17:22:21 +0530

    imported required modules

---
+++ main.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+import chromedriver_autoinstaller
+from selenium.webdriver.chrome.options import Options
+
+# Prevents the window from closing automaticallly after program is complete
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True)
+
+chromedriver_autoinstaller.install()
